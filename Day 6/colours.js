adf8aa920d42dbd24326ffb26172f569ad73527a
vd1992 <40485260+vd1992@users.noreply.github.com>
2018-10-16 20:01:09 -0600

    Polish exercise 6

--- Day 6/colours.js
+++ Day 6/colours.js
@@ -1,6 +1,6 @@
-//function that when given array of random colors, create divs and append to page
+//function that when given array of random colors in hex form, create divs and append to page
 //it also appends a text node with the hex value
-let pageFill=function(colors){
+let pageFill=function(colors,lums){
     for(let i of colors){
         //generate the new elements, a div to be coloured, and a text node with hex, apply styling
         let newBlock=document.createElement('div');
@@ -8,25 +8,32 @@ let pageFill=function(colors){
         newBlock.style.height="75px";
         newBlock.style.width="75px";
         newBlock.style.backgroundColor=i;
-        //pull containing div, append text to color div then append all as child to holder
+
+        //pull containing div, append text to color div, attach style class, and then append all as child to holder
         let swathHold=document.getElementById("swathHold");
         newBlock.appendChild(textHex);
-        swathHold.appendChild(newBlock);
+        newBlock.classList.add("color");
+
+        //but first, check if colors are dark, if so change text color, for best contrast/visibility
+        if(lums=="dark"){
+            newBlock.style.color="white";
+        }
+         swathHold.appendChild(newBlock);
     }
 }
 
 //random colour generator
 //get values from forms, run generator on button click, get results then pass into page filler function
 $('#subm').on('click', function() {
-    let forms=$('inputs');
+    //let forms=$('inputs');
     let nums=formColor.num.value;
     let hues=formColor.hues.value;
     let lums=formColor.lum.value;
-    //pass in requirements, get array called colors, pass into function
+    //pass in requirements, get array called colors which is hex values, pass into function
     let colors=randomColor({
             count:nums,
             hue:hues,
             luminosity:lums
     })
-    pageFill(colors);
+    pageFill(colors,lums);
 })
